c2c5f4b10258b213f6514ff8614e134300d5194e
Kévin Le Torc'h <kev29lt@gmail.com>
2019-05-08 21:36:53 +0200

    Working stuff

--- js/global.js
+++ js/global.js
@@ -5,7 +5,7 @@ let addItemToShoppingListForm = document.querySelector("#addItemToShoppingList")
 let shoppingItemName = document.querySelector("#shoppingItemName");                 // Item name location
 let shoppingList = document.querySelector("#shoppingList");                          // Shopping list location
 
-let itemSelected = false;
+let itemSelected = null;
 
 readShoppingListItem();
 
@@ -17,21 +17,25 @@ addItemToShoppingListForm.addEventListener("submit", (event) => {
     }
 });
 
-function addShopingListItem(name){
+function addShopingListItem(item){
     let liElement = document.createElement("li");
-    liElement.setAttribute("class", "list-group-item input-group");
+    liElement.setAttribute("class", "shoppingItem list-group-item input-group");
+    liElement.setAttribute("id", "shoppingItem_"+item.id);
 
-    liElement.innerHTML = name;
+    liElement.innerHTML = item.name;
 
     shoppingList.appendChild(liElement);
 }
 
 function createShoppingListItem(){
-    let itemName = shoppingItemName.value;
+    let item = {
+        id: shoppingListCollection.length,
+        name: shoppingItemName.value
+    };
 
-    if(itemName !== ""){                            // If the input fiel isn't empty
-        addShopingListItem(itemName);               // Add the item to the displayed list
-        shoppingListCollection.push(itemName);      // Add the item to the memory stored list
+    if(item.name !== ""){                            // If the input fiel isn't empty
+        addShopingListItem(item);               // Add the item to the displayed list
+        shoppingListCollection.push(item);      // Add the item to the memory stored list
         localStorage.setItem(                       // Replace the list by the one stored in memory
             "shoppingList", 
             JSON.stringify(shoppingListCollection)
@@ -44,11 +48,44 @@ function readShoppingListItem(){
 
     shoppingListCollection.forEach(element => {
         addShopingListItem(element);
+
+        let displayedShoppingList = document.querySelectorAll(".shoppingItem");
+        displayedShoppingList.forEach(element => {
+            element.addEventListener("click",
+                (event) => {
+                    if(element === itemSelected){
+                        shoppingItemName.value = "";
+                        itemSelected.setAttribute("class", "shoppingItem list-group-item input-group");
+                        itemSelected = null;
+                    }else{
+                        shoppingItemName.value=element.innerHTML;
+                        if(itemSelected) {
+                            itemSelected.setAttribute("class", "shoppingItem list-group-item input-group");
+                        }
+                        itemSelected = element;
+                        itemSelected.setAttribute("class", "shoppingItem list-group-item input-group active");
+                    }                    
+                }
+            );
+        });
+        
     });
 }
 
 function updateShoppingListItem(){
-    alert("Update !");
+    let item = {
+        id: itemSelected.id.split("_")[1],
+        name: shoppingItemName.value
+    };
+
+    if(item.name !== ""){                            // If the input fiel isn't empty
+        shoppingListCollection[item.id] = item;  // Add the item to the memory stored list
+        readShoppingListItem();                     // Refresh the displayed list
+        localStorage.setItem(                       // Replace the list by the one stored in memory
+            "shoppingList", 
+            JSON.stringify(shoppingListCollection)
+        );
+    }
 }
 
 function deleteShoppingListItem(){
