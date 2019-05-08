b5fca8d5446dbe63aa332f925754c3bcfec74c2d
Kévin Le Torc'h <kev29lt@gmail.com>
2019-05-08 19:04:07 +0200

    yeet

--- js/global.js
+++ js/global.js
@@ -3,22 +3,48 @@ let shoppingListCollection = JSON.parse(localStorage.getItem("shoppingList")) ||
 
 let addItemToShoppingListForm = document.querySelector("#addItemToShoppingList");   // Shopping item input form location
 let shoppingItemName = document.querySelector("#shoppingItemName");                 // Item name location
+let shoppingList = document.querySelector("#shoppingList");                          // Shopping list location
+
 let itemSelected = false;
 
+readShoppingListItem();
+
 addItemToShoppingListForm.addEventListener("submit", (event) => {
-    if(itemSelected){
-        updateShoppingListItem();
+    if(itemSelected){               // If an item is selected
+        updateShoppingListItem();   // We update it
     }else{
-        createShoppingListItem();
+        createShoppingListItem();   // We add it
     }
-})
+});
+
+function addShopingListItem(name){
+    let liElement = document.createElement("li");
+    liElement.setAttribute("class", "list-group-item input-group");
+
+    liElement.innerHTML = name;
+
+    shoppingList.appendChild(liElement);
+}
 
 function createShoppingListItem(){
     let itemName = shoppingItemName.value;
 
-    if(itemName !== ""){ // If the input fiel isn't empty
-        addShopingListItem(itemName);
+    if(itemName !== ""){                            // If the input fiel isn't empty
+        addShopingListItem(itemName);               // Add the item to the displayed list
+        shoppingListCollection.push(itemName);      // Add the item to the memory stored list
+        localStorage.setItem(                       // Replace the list by the one stored in memory
+            "shoppingList", 
+            JSON.stringify(shoppingListCollection)
+        );
     }
+}
+
+function readShoppingListItem(){
+    shoppingList.innerHTML = "";
+
+    shoppingListCollection.forEach(element => {
+        addShopingListItem(element);
+    });
 }
 
 function updateShoppingListItem(){
