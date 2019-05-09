95bf84213856441ad0e92e82da98ec117626d721
Kévin Le Torc'h <kev29lt@gmail.com>
2019-05-09 22:43:18 +0200

    FR COMMENTS

--- js/global.js
+++ js/global.js
@@ -19,46 +19,49 @@ addItemToShoppingListForm.addEventListener("submit", (event) => {       // Quand
 
 deleteShoppingListItemButton.addEventListener("click", (event) =>{                   // Quand on clique sur la corbeille
     if(itemSelected){                                                                // Si un article est sélectionné
-        if(confirm("Do you want to delete the item "+itemSelected.innerHTML+" ?")){  // On de
-            deleteShoppingListItem();
+        if(confirm("Do you want to delete the item "+itemSelected.innerHTML+" ?")){  // On demande la confirmation de la suppression de l'article
+            deleteShoppingListItem();                                                // Appel de la fonction de suppression
         }
     }
 });
 
-function indexOfItemById(array, id){
-    let i = -1;
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        if(element.id == id){
-            i = index;
+function indexOfItemById(array, id){                        // On récupère la position dans un tableau d'un article avec un identifiant donné
+    let i = -1; // Par défaut on est en dehors du tableau
+    for (let index = 0; index < array.length; index++) {    // On parcours le tableau donné                         
+        if(array[index].id == id){                          // Si l'identifiant de l'article parcouru est le même que l'identifiant donné
+            i = index;                                      // On stocke dans i la position de l'article correspondant
         }
     }
 
-    return i;
+    return i;   // On retourne l'identifiant trouvé
 }
 
-function searchForHighestIndex(array){
-    let id = -1;
+function searchForHighestIndex(array){      // On cherche dans le tableau l'élément qui as l'identifiant le plus grand
+    let id = -1;                            // Par défaut on est en dehors du tableau
 
-    array.forEach(element => {
-        if(element.id > id){
-            id = element.id;
+    array.forEach(element => {              // On parcours le tableau pour chaque élément
+        if(element.id > id){                // Si l'identifiant de l'élément parcouru est plus grand que celui stocké dans id
+            id = element.id;                // On le stocke dans id
         }
     });
 
-    return id;
+    return id;  // On retourne la valeur de l'identifiant le plus grand 
 }
 
-function addShopingListItem(item){
-    shoppingList.innerHTML += "<li style=\"list-style-type:none\" type=\"hidden\"></li>";
+function addShopingListItem(item){     // Ajoute à la liste non ordonnée (<ul>) shoppingList un article
+    /*  Suite à un problème dont je n'ai pas encore la solution, Je ne pouvais pas sélectionner une ligne sur deux.
+        Dans le cas d'un nombre d'article pair, les articles sélectionnables étaient ceux à des positions paires et
+        inversement dans le cas d'un nombre d'articles impair, pour résoudre ce problème rapidement :*/
+    shoppingList.innerHTML += "<li style=\"list-style-type:none\" type=\"hidden\"></li>";  // J'ai ajouté un balise <li> de type "hidden" (caché) avant celle de l'article affiché
+    /* Ce n'est pas une solution très propre mais elle marche, je vais essayer de trouver mieux */ 
 
-    let liElement = document.createElement("li");                                   // Creates a <li> tag
-    liElement.setAttribute("class", "shoppingItem list-group-item input-group");    // Adds class attributes
-    liElement.setAttribute("id", "shoppingItem_"+item.id);                          // Adds id attributes
+    let liElement = document.createElement("li");                                   // On crée une balise <li>
+    liElement.setAttribute("class", "shoppingItem list-group-item input-group");    // On lui des attributs de classe 
+    liElement.setAttribute("id", "shoppingItem_"+item.id);                          // On lui ajoute un identifiant
 
-    liElement.innerHTML = item.name;                                                // Adds the item name inside the <li> tag
+    liElement.innerHTML = item.name;                                                // On rajoute le nom de l'article dans la balise <li>
 
-    shoppingList.appendChild(liElement);                                            // Adds the li tag inside the shoppingList
+    shoppingList.appendChild(liElement);                                            // On rajoute la balise <li> dans la liste non ordonnée (<ul>) shoppingList
 }
 
 function createShoppingListItem(){
